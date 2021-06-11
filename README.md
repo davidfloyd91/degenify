505f5fd343eff762677e39c8c9826595021f0826
davidfloyd91 <davidfloyd91@gmail.com>
2021-06-11 01:00:34 -0400

    deployed

---
+++ README.md
@@ -0,0 +1,3 @@
+## Degenify
+
+Deployed at `0xcc3eB2EF87B6872b802b58A98BcACe061Fe054dF`
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -3,3 +3,8 @@ from brownie import Degenify, accounts
 def main():
     acct = accounts.load('deploy_account')
     acct.deploy(Degenify)
+
+# Transaction sent: 0x20988deeed2ce5fb6fda414d2f986d7c989da87e3f6a17c6045a585ee67d62d3
+#   Gas price: 10.000001459 gwei   Gas limit: 1812860   Nonce: 0
+#   Degenify.constructor confirmed - Block: 12611149   Gas used: 1648055 (90.91%)
+#   Degenify deployed at: 0xcc3eB2EF87B6872b802b58A98BcACe061Fe054dF
